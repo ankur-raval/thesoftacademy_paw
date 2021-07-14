933d07778f5c5d44fc82d118b485b0231717f66f
ankur-raval <61215439+ankur-raval@users.noreply.github.com>
2021-07-14 11:50:00 +0530

    v1.1

--- pages_app/urls.py
+++ pages_app/urls.py
@@ -5,7 +5,22 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.index_view,name='index'), 
+    path('',views.index_view,name='index'),
+    path('courses',views.courses_view,name='courses'),
+    path('free_session',views.free_session_view,name='free_session'),
+    path('basic_course',views.basic_course_view,name='basic_course'),
+    path('advance_course',views.advance_course_view,name='advance_course'),
+    path('events',views.events_view,name='events'),
+    path('blogs',views.blogs_view,name='blogs'),
+    path('contact',views.contact_view,name='contact'),
+    path('services',views.services_view,name='services'),
+
+
+
+
+
+
+
 
     
 
--- pages_app/views.py
+++ pages_app/views.py
@@ -4,3 +4,30 @@ from django.shortcuts import render, redirect
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def courses_view(request):
+    return render(request, 'courses.html')
+
+def free_session_view(request):
+    return render(request, 'courses_list/free_session.html')
+
+def basic_course_view(request):
+    return render(request, 'courses_list/basic_course.html')
+
+def advance_course_view(request):
+    return render(request, 'courses_list/advance_course.html')
+
+def events_view(request):
+    return render(request, 'events.html')
+
+def blogs_view(request):
+    return render(request, 'blogs.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def services_view(request):
+    return render(request, 'services.html')
+
+
